27d76755ecc9844c2f80571b6837a4c48a46cd3a
Karm-Mavani <karmmavani63@gmail.com>
2024-05-09 17:46:35 +0530

    09/05/2024 -C language

---
+++ ch-1/1_2/2.C
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+
+main()
+{
+
+	clrscr();
+
+	printf("--------\n");
+	printf("|      |\n");
+	printf("R      |\n");
+	printf("N      |\n");
+	printf("W      |\n");
+	printf("|      |\n");
+	printf("--------");
+
+
+	getch();
+
+
+}
---
+++ ch-1/1_2/FIRST.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+
+	printf("name\t:karm\n");
+	printf("age\t:15\n");
+	printf("school\t:p.p. savani chaitanya vidhya sankul")
+
+	getch();
+}
